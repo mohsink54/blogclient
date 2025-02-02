136e8a234633cf5f438b6b1852b6dd17182102fa
mohsink54 <mohsin.jsdeveloper@gmail.com>
2025-02-02 09:44:07 +0500

    Fix Vercel deployment

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
     env: {
         imageKit: {
             publicKey: import.meta.env.VITE_IMAGEKIT_PUBLIC_KEY,
-            urlEndpoint: import.meta.env.VITE__IMAGEKIT_URL_ENDPOINT,
+            urlEndpoint: import.meta.env.VITE_IMAGEKIT_URL_ENDPOINT,
         },
     },
 };
